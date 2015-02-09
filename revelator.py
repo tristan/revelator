7b07b1dfe61bc23f5ee7e024dcc669a473f8c611
Tristan King <tristan.king@gmail.com>
2015-02-09 19:35:31 +0100

    adjusted times for australian daylight savings

--- revelator.py
+++ revelator.py
@@ -24,32 +24,43 @@ DAYS = [
     '2014-09-15',
     '2014-09-22',
     '2014-09-29',
-    '2014-10-06',
-    '2014-10-13',
-    '2014-10-20',
-    '2014-10-27',
-    '2014-11-03',
-    '2014-11-10',
-    '2014-11-17',
-    '2014-11-24',
-    '2014-12-01',
-    '2014-12-08',
-    '2015-01-12',
+    # is everything past here in daylight savings time?!
+    ('2014-10-06', -1),
+    ('2014-10-13', -1),
+    ('2014-10-20', -1),
+    ('2014-10-27', -1),
+    ('2014-11-03', -1),
+    ('2014-11-10', -1),
+    ('2014-11-17', -1),
+    ('2014-11-24', -1),
+    ('2014-12-01', -1),
+    ('2014-12-08', -1),
+    ('2015-01-12', -1),
     #'2015-01-19', (was beat the drum)
-    '2015-01-26',
-    '2015-02-02',
-    '2015-02-09',
+    ('2015-01-26', -1),
+    ('2015-02-02', -1),
+    ('2015-02-09', -1)
 ]
 
 false_positives = [
     'spotify:track:4b5Xvdwe76VHiwvtkmsfaa'
 ]
 
-if __name__ == '__main__':
+default_start = 10
+default_end = 12
 
+if __name__ == '__main__':
+    
     global_tracks = []
     for day in DAYS:
-        tracks = generate_playlist("doublej", day, "10:00:00.000", "12:00:00.000")
+        if type(day) == tuple:
+            day, offset = day
+            start_time = default_start + offset
+            end_time = default_end + offset
+        else:
+            start_time = default_start
+            end_time = default_end
+        tracks = generate_playlist("doublej", day, "%s:00:00.000" % start_time, "%s:00:00.000" % end_time)
         with open('revelator_%s.spotify' % day, 'w') as f:
             for href in tracks:
                 if href not in global_tracks:
